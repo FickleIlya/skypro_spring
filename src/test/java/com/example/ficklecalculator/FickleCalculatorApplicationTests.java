d8b749edd1ea82108878936a50597740ec3b3834
FickleIlya <fickle041@gmail.com>
2022-09-16 13:29:05 +0300

    init commit

---
+++ src/test/java/com/example/ficklecalculator/FickleCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ficklecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FickleCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
